275b763b72be587aa12ff4466c16ac4a40453018
awand795 <awand795@gmail.com>
2020-04-22 14:56:27 +0700

    Adding SPA Handling in backend

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.use(cors());
 
 app.use('/api/catatan',catatanController);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname+"/views/"));
+    app.get(/.*/,(req,res)=> res.sendFile(__dirname+'/views/index.html'));
+}
+
 app.listen(PORT,()=>{
     console.log(`Application running on port ${PORT}`);
 });
