58268469d2c2e7986998c0687b196d88397ec5de
awand795 <awand795@gmail.com>
2020-04-22 09:59:24 +0700

    Changing Local MongoDB to MongoDB Atlas For Backend

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-//mongoose.connect('mongodb+srv://awand795:awanda21345@cluster0-i1501.mongodb.net/note?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology : true},(err)=>{
-    mongoose.connect('mongodb://localhost:27017/note',{useNewUrlParser:true, useUnifiedTopology : true},(err)=>{
+mongoose.connect('mongodb+srv://awand795:awanda21345@cluster0-i1501.mongodb.net/note?retryWrites=true&w=majority',{useNewUrlParser:true, useUnifiedTopology : true},(err)=>{
     if(err){
         console.log(err);
     }
